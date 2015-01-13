f31235bd97891e6ed62d2d30dcfb72163cbd0711
melsov <melsovereign@yahoo.com>
2015-01-13 10:19:21 -0500

    second commit

---
+++ src/somesingleton/TheSun.java
@@ -0,0 +1,29 @@
+package somesingleton;
+
+public final class TheSun {
+	private int angleInSky;
+	private static TheSun theSun;
+	
+	//making the constructor private
+	//means that no other class can instantiate a new sun
+	//(draw-back: also means that the sun can't be sub-classed)
+	private TheSun() {
+	}
+	
+	
+	public static TheSun GetTheSun() {
+		if(theSun == null) {
+			theSun = new TheSun();
+		}
+		return theSun;
+	}
+	
+	public void update(int timeOfDayInMinutes) {
+		angleInSky = timeOfDayInMinutes * (180/720); // or whatever
+	}
+	
+	public boolean isShining() {
+		return angleInSky < 180;
+	}
+
+}
